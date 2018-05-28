d569f72b4e28573f0e28b2641fa174cb0d726e8a
anavarro-wellnet <alberto.navarro@wellnet.it>
2018-05-28 16:25:05 +0200

    modificato container generale, modificati pading griglia generale, implemementata base per la gestione della sidebar

---
+++ js/sidebar_nav.js
@@ -0,0 +1,16 @@
+// Sidebar nav
+module.exports = themeSidebarNav = (function ($) {
+  var that;
+
+  return {
+
+    $: {
+
+    },
+
+    init: function(callback) {
+      that = this.$;
+    }
+  }
+
+})(jQuery);
