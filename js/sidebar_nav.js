2f1f730cf17ecf6ab49703bde3f59d4e89656ca8
anavarro-wellnet <alberto.navarro@wellnet.it>
2018-05-29 11:19:04 +0200

    fix grunt file, fix main wrapper, add sidebar nav desktop/mobile

--- js/sidebar_nav.js
+++ js/sidebar_nav.js
@@ -5,11 +5,27 @@ module.exports = themeSidebarNav = (function ($) {
   return {
 
     $: {
-
+      $toggle: $('.navbar-toggler'),
+      $categorymenu: $('.sidebar-btn--wrap-mobile'),
+      $nav: $('.navbar-collapse')
     },
 
     init: function(callback) {
       that = this.$;
+      themeSidebarNav.displaynavCategory();
+    },
+
+    displaynavCategory: function() {
+      // There is no event to know when offanvas is open or close 'shown.bs.collapse' not work.
+      // The buttons at the top is outside offCanvas becouse they are in fixed position and offCanvas is in fixed position too
+
+      that.$toggle.on('click', function () {
+        if(that.$nav.hasClass('open')) {
+          that.$categorymenu.addClass('active');
+        } else {
+          that.$categorymenu.removeClass('active');
+        }
+      });
     }
   }
 
