6c9c1ba2aa5696b8a5f2ae6ae93d49c1bdc692ac
anavarro-wellnet <alberto.navarro@wellnet.it>
2018-05-29 16:21:21 +0200

    separato comportamento sidebar desktop ( accordion ) e mobile ( tab )

--- js/sidebar_nav.js
+++ js/sidebar_nav.js
@@ -5,27 +5,29 @@ module.exports = themeSidebarNav = (function ($) {
   return {
 
     $: {
-      $toggle: $('.navbar-toggler'),
-      $categorymenu: $('.sidebar-btn--wrap-mobile'),
-      $nav: $('.navbar-collapse')
+      $openToggle: $('.navbar-toggler--open'),
+      $backToggle: $('.navbar-toggler--back'),
+      $tab: $('.sidebar-tabs'),
+      $nav: $('.docs-italia-offcanvas-menu')
     },
 
     init: function(callback) {
       that = this.$;
-      themeSidebarNav.displaynavCategory();
+      themeSidebarNav.displayNav();
     },
 
-    displaynavCategory: function() {
-      // There is no event to know when offanvas is open or close 'shown.bs.collapse' not work.
-      // The buttons at the top is outside offCanvas becouse they are in fixed position and offCanvas is in fixed position too
+    displayNav: function() {
 
-      that.$toggle.on('click', function () {
-        if(that.$nav.hasClass('open')) {
-          that.$categorymenu.addClass('active');
-        } else {
-          that.$categorymenu.removeClass('active');
-        }
+      that.$openToggle.on('click', function () {
+        that.$tab.addClass('active');
+        that.$nav.addClass('active');
       });
+
+      that.$backToggle.on('click', function() {
+        that.$tab.removeClass('active');
+        that.$nav.removeClass('active');
+      });
+
     }
   }
 
