7333e35eb87964fafd165f6881f456c4faee644d
anavarro-wellnet <alberto.navarro@wellnet.it>
2018-05-31 10:18:43 +0200

    implementato js per aprire l'accordion relativo alla voce selezionata e aggiunta classe current-item alla voce di menu corrispondente

--- docs_italia_theme/static/js/theme.js
+++ docs_italia_theme/static/js/theme.js
@@ -652,7 +652,7 @@ module.exports = themeSidebarNav = (function ($) {
       $tab: $('.sidebar-tabs'),
       $nav: $('.docs-italia-offcanvas-menu'),
       $navItem: $('.sidebar-list--wrapper a'),
-      $currentItem: {}
+      $currentItem: {} // last menù item selected
     },
 
     init: function(callback) {
@@ -663,6 +663,7 @@ module.exports = themeSidebarNav = (function ($) {
       themeSidebarNav.selectAccordion();
     },
 
+    // Offcanvas nav
     displayNav: function() {
       that.$openToggle.on('click', function () {
         that.$tab.addClass('active');
@@ -675,42 +676,59 @@ module.exports = themeSidebarNav = (function ($) {
       });
     },
 
+    // Select current inten on click ( nav in same page).
     addHandler: function() {
       that.$navItem.on('click', function(){
         that.$navItem.not($(this)).removeClass('current_item');
         $(this).addClass('current_item');
+
+        // Close offcanvas on click
+        if ( $( window ).width() <= 767 ) {
+          that.$backToggle.trigger('click');
+        }
       });
     },
 
+    // Select the right current item on page load.
     selectNavItemOnLoad: function() {
       var location = window.location.href,
           hashtagIndex = location.indexOf("#"),
           hash = location.substring(hashtagIndex,location.length);
 
-      that.$currentItem =
-        that.$navItem.filter(
-          function(index) {
-            var item = $(this),
-                thisHashIndex = item.attr('href').indexOf("#"),
-                thisHash = item.attr('href').substring(thisHashIndex,thisHashIndex.length);
-            return thisHash == hash;
-          });
+      that.$currentItem = that.$navItem.filter(function(index) {
+        var item = $(this),
+            thisHashIndex = item.attr('href').indexOf("#"),
+            thisHash = item.attr('href').substring(thisHashIndex,thisHashIndex.length);
 
+        return thisHash == hash;
+      });
+
+      // Check if item doesn't have hash on url, select the item with href = "#" ( <li class="toctree-l1 current"> )
+      if(that.$currentItem.length == 0) {
+        that.$currentItem = that.$navItem.filter(function(index){
+          return $(this).attr('href') == '#'
+        })
+      }
       that.$currentItem.addClass('current_item');
     },
 
+    // Open the right accordion on page loaded, depends on the current item.
     selectAccordion: function() {
-      var $activeList = that.$currentItem.closest('.sidebar-list--wrapper'), // CLASSE PIU SPECIFICA DESKTOP TO DO
-          $activeListMobile = that.$currentItem.closest('.tab-pane'),
+          // Detect desktop accordion.
+      var $activeList = that.$currentItem.closest('.sidebar-accordion-content'),
           id = $activeList.attr('id'),
-          idMobile = $activeListMobile.attr('id'),
           $listBtn = $('.sidebar-btn').filter(function(){
             return $(this).attr('data-target') == '#' + id
           }),
+
+          // Detect mobile tab.
+          $activeListMobile = that.$currentItem.closest('.tab-pane'),
+          idMobile = $activeListMobile.attr('id'),
           $listBtnMobile = $('.sidebar-btn--mobile').filter(function(){
             return $(this).attr('href') == '#' + idMobile
           });
 
+      // Trigger click on accordion or tab.
       if ( $( window ).width() > 768 ) {
         if( $listBtn.hasClass('collapsed') ) $listBtn.trigger('click');
       } else {
