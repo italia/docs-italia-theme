6cb41fc95ebcca8ff8ba7a25ec0129ddb2d3ab06
nino.marrazzo@wellnet.it <nino.marrazzo@wellnet.it>
2018-06-27 11:43:47 +0200

    AGID-322 Integrazione con Discourse fuori dall'ambiente di sviluppo - Aggiunge path per direttiva nel setup-py

--- docs_italia_theme/__init__.py
+++ docs_italia_theme/__init__.py
@@ -227,6 +227,6 @@ def setup(app):
     app.connect('doctree-resolved', generate_glossary_json)
     app.connect('doctree-resolved', glossary_page_id)
     app.add_html_theme('docs_italia_theme', os.path.abspath(os.path.dirname(__file__)))
-    
+
     # Setup for discourse_comments custom directive
     discourse_comments.setup(app)
