eee33bbf7809f5a01aaaa7460f2b52da85a297d8
Davide Porrovecchio <pdavide@gmail.com>
2018-08-29 18:10:50 +0200

    Improve backward compatibility

--- docs-italia-theme/__init__.py
+++ docs-italia-theme/__init__.py
@@ -3,8 +3,12 @@
 
 """Docs Italia theme"""
 
+import os
+
 def get_html_theme_path():
-    return None
+    """Return list of HTML theme paths."""
+    cur_dir = os.path.abspath(os.path.dirname(__file__) + '../docs_italia_theme')
+    return cur_dir
 
 def setup(app):
     pass
