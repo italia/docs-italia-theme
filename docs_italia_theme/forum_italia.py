1246ed42dafd4e3f5f534dd300e1815a33778ce8
Davide Porrovecchio <pdavide@gmail.com>
2019-05-23 09:31:31 +0200

    Remove unused code

--- docs_italia_theme/forum_italia.py
+++ docs_italia_theme/forum_italia.py
@@ -33,31 +33,7 @@ class ForumItaliaCommentsDirective(Directive):
             <div class="forum-italia-comments" data-topic='""" + options['topic_id'] + """'></div>
         """
 
-        # Create "account suspended" modal
-        suspended_modal = """
-            <div class="modal suspended-modal" id="suspended-modal" tabindex="-1" role="dialog">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title">Account sospeso</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">
-                    <p>Il tuo account e&grave; stato sospeso, se ti serve aiuto vieni a trovarci sul workspace Slack di Developers Italia.</p>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal" id="logout-modal__cancel">Chiudi</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-        """
-
         node += nodes.raw(text=topic_markup, format='html')
-        node += nodes.raw(text=suspended_modal, format='html')
-
         node['data-topic-id'] = options['topic_id']
 
         return [ node ]
